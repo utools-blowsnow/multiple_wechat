bfb55928638ecbb9e8820f939150aa966b429b9d
blowsnow <im.blowsnow@gmail.com>
2022-09-20 22:59:38 +0800

    修复微信wechat files目录修改问题

--- preload.js
+++ preload.js
@@ -2,19 +2,18 @@ const fs = require("fs");
 let documents = window.utools.getPath('documents');
 const path = require('path')
 
-let configPath = documents + "\\WeChat Files\\All Users\\config\\config.data";
-
-
-
-function startWx(wxid=0){
-
-    // 重新登陆一个新的微信账号
-    if (fs.existsSync(configPath)) fs.unlinkSync(configPath);
-
-    if (wxid && fs.existsSync(documents + "\\WeChat Files\\All Users\\config\\"+wxid+".data")){
-        fs.copyFileSync(documents + "\\WeChat Files\\All Users\\config\\"+wxid+".data",configPath);
+let wechatFilePath = documents + "\\WeChat Files";
+if (!fs.existsSync(wechatFilePath)){
+    let wechatFilePathData = window.utools.db.get("wechatFilePath");
+    if (wechatFilePathData){
+        wechatFilePath = wechatFilePathData.data;
     }
+}
 
+function configPath(){
+    return wechatFilePath + "\\All Users\\config\\config.data";
+}
+function getExePath(){
     let exeData = window.utools.db.get("multiple_wechat");
     let exePath = null;
 
@@ -30,7 +29,7 @@ function startWx(wxid=0){
         if (!list || list.length === 0){
             window.utools.copyText('https://github.com/utools-blowsnow/multiple_wechat/raw/master/multiple_wechat.exe')
             window.utools.showNotification("设置失败，多开程序下载地址已复制到剪切板");
-            return;
+            return false;
         }
 
         exePath = list[0];
@@ -38,7 +37,7 @@ function startWx(wxid=0){
         if (exePath.indexOf("multiple_wechat.exe") === -1){
             window.utools.copyText('https://github.com/utools-blowsnow/multiple_wechat/raw/master/multiple_wechat.exe')
             window.utools.showNotification("请下载多开程序，下载地址已复制到剪切板");
-            return;
+            return false;
         }
 
         window.utools.db.put({
@@ -50,29 +49,71 @@ function startWx(wxid=0){
         exePath =  exeData.data;
     }
 
+    return exePath;
+}
+function getWechatFilesPath(){
+    if (!fs.existsSync(wechatFilePath)){
+        let list = window.utools.showOpenDialog({
+            title: "请选择微信文档 WeChat Files 目录",
+            properties: ['openDirectory']
+        })
+        if (!list || list.length === 0){
+            window.utools.showNotification("选择目录失败");
+            return false;
+        }
+
+        wechatFilePath = list[0];
+
+        if (wechatFilePath.split("\\").pop() !== "WeChat Files"){
+            window.utools.showNotification("请选择微信文档 WeChat Files 目录");
+            return false;
+        }
+
+        window.utools.db.put({
+            _id: "wechatFilePath",
+            data: wechatFilePath
+        })
+    }
+
+    return wechatFilePath;
+}
+
+function startWx(wxid=0){
+    // 重新登陆一个新的微信账号
+    if (fs.existsSync(configPath())) fs.unlinkSync(configPath());
+
+    if (wxid && fs.existsSync(wechatFilePath + "\\All Users\\config\\"+wxid+".data")){
+        fs.copyFileSync(wechatFilePath + "\\All Users\\config\\"+wxid+".data",configPath());
+    }
+
+    let exePath = getExePath();
+    if (!exePath) return false;
+
     // 启动微信
     window.utools.shellOpenPath(exePath);
-
-    setTimeout(() => {
-        window.utools.showNotification("登陆成功后，不要忘记确认登陆哦！");
-    },10 * 1000)
 }
 function loadWxData(){
+    if (!fs.existsSync(configPath())){
+        window.utools.showNotification("未登录，无法获取登陆信息");
+        return false;
+    }
+
+
     // \WeChat Files\All Users\config\config.data
-    let configText = fs.readFileSync(configPath, 'utf8');
-    let matches = configText.match(/\\WeChat Files\\(.*?)\\config/);
-    let wxid = matches[1];
+    let configText = fs.readFileSync(configPath(), 'utf8');
+    let matches = configText.match(/wxid_[a-zA-Z0-9]*/);
+    if (!matches) return false;
+    let wxid = matches[0];
     // 获取到wxid了
 
-
     // 获取账号名称
-    let accInfoPath = documents + "\\WeChat Files\\" + wxid + "\\config\\AccInfo.dat";
+    let accInfoPath = wechatFilePath + "\\" + wxid + "\\config\\AccInfo.dat";
     let accInfoText = fs.readFileSync(accInfoPath, 'utf8');
     let accInfoMatches = accInfoText.match(/http:\/\/wx.qlogo.cn[a-zA-Z0-9_/]+/);
     let wxPic = accInfoMatches[0];
 
     // 获取微信名称
-    let fileObjs = fs.readdirSync(documents + "\\WeChat Files\\" + wxid);
+    let fileObjs = fs.readdirSync(wechatFilePath + "\\" + wxid);
     let wxName = "获取名称失败："  + wxid;
     for (const fileObj of fileObjs) {
         if (fileObj.startsWith("account_")){
@@ -90,13 +131,16 @@ function loadWxData(){
 
 }
 function saveWxData(){
+    if (!getWechatFilesPath()){
+        return false;
+    }
+
     let wxData = loadWxData();
-    if (!wxData.id){   // 获取失败了
+    if (!wxData || !wxData.id){   // 获取失败了
         return false;
     }
     // 备份一次下次快捷登陆使用
-    fs.copyFileSync(configPath,documents + "\\WeChat Files\\All Users\\config\\"+wxData.id+".data");
-
+    fs.copyFileSync(configPath(),wechatFilePath + "\\All Users\\config\\"+wxData.id+".data");
 
     // 记录本次登陆的微信账号信息
     window.utools.db.put({
@@ -107,6 +151,8 @@ function saveWxData(){
     return wxData;
 }
 
+window.saveWxData = saveWxData;
+window.startWx = startWx;
 
 window.exports = {
     "wechat_list": { // 注意：键对应的是 plugin.json 中的 features.code
@@ -180,11 +226,7 @@ window.exports = {
             enter: (action) => {
                 window.utools.hideMainWindow()
 
-                try {
-                    startWx(0);
-                }catch (e){
-                    window.utools.showNotification("错误" + e.message);
-                }
+                startWx(0);
 
                 window.utools.outPlugin();
             }
@@ -196,11 +238,17 @@ window.exports = {
             // 进入插件应用时调用
             enter: (action) => {
                 window.utools.hideMainWindow()
+                let data = null;
+                try {
+                    data = saveWxData();
+                }catch (e){
+                    // window.utools.showNotification("保存失败：" + e.message);
+                    //
+                    // fs.writeFileSync("C:\\error.txt", JSON.stringify(e, Object.getOwnPropertyNames(e), 2));
 
-                let data = saveWxData();
+                }
                 if (data === false){
                     window.utools.showNotification("保存微信账号失败");
-                    return;
                 }else{
                     window.utools.showNotification("保存微信账号成功：" + data.name);
                 }
