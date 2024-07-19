dc89cdd2ee62a60e38073deef139dcbc6ad855bc
feixue <feixue@esign.cn>
2024-07-19 11:39:52 +0800

    init

--- preload.js
+++ preload.js
@@ -77,6 +77,69 @@ window.exports = {
             placeholder: "搜索"
         }
     },
+    "wechat_delete_list": { // 注意：键对应的是 plugin.json 中的 features.code
+        mode: "list",  // 列表模式
+        args: {
+            // 进入插件应用时调用（可选）
+            enter: (action, callbackSetList) => {
+                // 获取记录的微信列表
+                let docs = window.utools.db.allDocs("wx_");
+
+                let list = [];
+                for (let item of docs) {
+                    if (!item._id.includes(utools.getNativeId())){
+                        continue
+                    }
+                    let data = JSON.parse(item.value);
+                    list.push({
+                        key: item._id,
+                        title: data.name,
+                        description: data.id,
+                        icon: data.logo,
+                        id: data.id
+                    })
+                }
+                // 如果进入插件应用就要显示列表数据
+                callbackSetList(list)
+            },
+            // 子输入框内容变化时被调用 可选 (未设置则无搜索)
+            search: (action, searchWord, callbackSetList) => {
+                // 获取一些数据
+                let docs = window.utools.db.allDocs("wx_");
+                let list = [];
+                for (let item of docs) {
+                    if (!item._id.includes(utools.getNativeId())){
+                        continue
+                    }
+                    let data = JSON.parse(item.value);
+                    if (data.name.indexOf(searchWord) !== -1) {
+                        list.push({
+                            key: item._id,
+                            title: data.name,
+                            description: data.id,
+                            icon: data.logo,
+                            id: data.id
+                        })
+                    }
+                }
+
+                // 执行 callbackSetList 显示出来
+                callbackSetList(list)
+            },
+            // 用户选择列表中某个条目时被调用
+            select: async (action, itemData, callbackSetList) => {
+                window.utools.hideMainWindow()
+                try {
+                    utools.dbStorage.removeItem(itemData.key);
+                } catch (e) {
+                    window.utools.showNotification("删除失败：" + e.message);
+                }
+                window.utools.outPlugin()
+            },
+            // 子输入框为空时的占位符，默认为字符串"搜索"
+            placeholder: "搜索"
+        }
+    },
     "wechat_start": {
         mode: "none",
         args: {
